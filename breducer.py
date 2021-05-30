ffc95547c319135e62cd3eb8efce2b568966184b
Razor <raza9999ali@gmail.com>
2021-05-30 02:17:17 -0400

    in process part a

---
+++ breducer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import sys
+
+previous = None
+sum = 0
+
+input = open("unigrams.txt", 'w')
+for line in sys.stdin:
+    key,value =  line.split( '\t' )
+
+    if key != previous:
+        if previous is not None:
+           input.write ( str( sum ) + '\t' + previous + '\n' )
+        previous = key
+        sum = 0
+   
+    sum = sum + int(value)
+
+input.write( str(sum) + '\t' + previous + '\n')
+input.close()
