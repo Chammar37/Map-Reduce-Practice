259a58e7d7d46ccdea8aa1ae1609f105c0a282c8
Razor <raza9999ali@gmail.com>
2021-05-30 02:10:02 -0400

    umapper

--- umapper.py
+++ umapper.py
@@ -1,24 +1,28 @@
 #!/usr/bin/python3
 import sys
 import re
+import nltk
 from nltk.tokenize import RegexpTokenizer
+from nltk.tokenize import word_tokenize
+from nltk import ngrams
+# nltk.download('punkt')
 
-# input = open("tester_1.txt", 'w')
+input = open("tester_2.txt", 'w')
 
 for line in sys.stdin:
     line = re.sub('"', '', line)
 
     words = line.split(",")
+    
+    tokenizer = RegexpTokenizer("[\w']+")
+    # # [\w']+
 
     comments = []
-    comments.append(words[0])
+    comments.append(tokenizer.tokenize(words[0]))
 
-    tokenizer = RegexpTokenizer("[\w']+") 
-    # [\w']+
-    words = tokenizer.tokenize(comments[0])
+    for words in comments:
+        for word in words:
+            input.write( word + "\t1" + "\n")
+            # print(str(word) + "\t1")
 
-    for word in words:
-        # input.write( word.lower() + "\t1" + "\n")
-        print(word.lower() + "\t1")
-
-# input.close()
+input.close()
